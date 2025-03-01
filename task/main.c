8c496b30dda494e54aeeac5512313c9076c40e6d
AHMED-SHERIF0 <as01288623151@gmail.com>
2025-03-01 21:47:22 +0200

    Hello Github

---
+++ task/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   char name[60];
+   printf("enter your name please ");
+   scanf(" %s",&name);
+   printf("congrat %s You Learnt How To Use Github",name);
+    return 0;
+}
